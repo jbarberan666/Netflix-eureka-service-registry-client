1223fc3388938cbd39391d0e290e3c8811195ee8
jbarberan <jba@LAPTOP-5253SEOI.localdomain>
2021-03-04 21:43:39 +0100

    add project content

---
+++ src/test/java/fr/jba/serviceregistrationanddiscoveryclient/ServiceRegistrationAndDiscoveryClientApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.jba.serviceregistrationanddiscoveryclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistrationAndDiscoveryClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
